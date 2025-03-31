dc7cbaceddf11a5701c5a024babe6930faf295c5
WARE <138410028+WARE8165@users.noreply.github.com>
2025-03-31 15:08:15 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+alert("Bem-Vindo ao HOPXY!")
+alert("Criado por TheOldWare!")
+alert("Todos os jogos são verificados e seguros!")
+alert("doações e suporte disponivel!")
+
 window.onload = function(){
     var audio = document.getElementById('Audio');
     audio.muted = false;
